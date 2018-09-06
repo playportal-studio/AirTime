f7e661cbec962861dd32d29fa0d5fa307aef07e4
J. Edward Black <black.j.edward@gmail.com>
2018-09-06 09:28:57 -0400

    Initial Commit

---
+++ AirTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirTime
+//
+//  Created by Jett Black on 9/6/18.
+//  Copyright © 2018 Dynepic, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
