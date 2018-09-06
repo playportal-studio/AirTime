a17da9ce3d244180b687f0b0182d485372c0706e
J. Edward Black <black.j.edward@gmail.com>
2018-09-06 10:37:22 -0400

    vc is now ktmdevicesmanager delagate & connecting to peripheral

--- AirTime/ViewController.swift
+++ AirTime/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import KontaktSDK
+
+let peripheralUUID = "HsDujU"
 
 class ViewController: UIViewController {
-
+    
+    var devicesManager: KTKDevicesManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        devicesManager = KTKDevicesManager(delegate: self)
+        devicesManager.startDevicesDiscovery()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: KTKDevicesManagerDelegate {
+    func devicesManager(_ manager: KTKDevicesManager, didDiscover devices: [KTKNearbyDevice]) {
+        print("Did discover: \(devices)")
+        guard let device = devices.filter({$0.uniqueID == peripheralUUID}).first else { return }
+        let connection = KTKDeviceConnection(nearbyDevice: device)
+        print("Discovered with connection: \(connection)")
+        
+    }
+}
