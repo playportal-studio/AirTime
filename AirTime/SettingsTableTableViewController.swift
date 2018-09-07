6d591c5d88e0700c3dbb82aac5c40a9566fc059c
J. Edward Black <black.j.edward@gmail.com>
2018-09-07 15:04:52 -0400

    table view cells

--- AirTime/SettingsTableTableViewController.swift
+++ AirTime/SettingsTableTableViewController.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class SettingsTableTableViewController: UITableViewController {
+    
+    @IBOutlet weak var cell1: UITableViewCell!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+       cell1.textLabel!.text = "Whpaseoifhasdolf"
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,15 +27,15 @@ class SettingsTableTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
