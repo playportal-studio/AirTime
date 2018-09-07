cb7a78445564f6e7fac97eed0a76756fb100068d
J. Edward Black <black.j.edward@gmail.com>
2018-09-07 11:37:42 -0400

    kljsdhfkds

--- AirTime/AppDelegate.swift
+++ AirTime/AppDelegate.swift
@@ -15,9 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let font = UIFont(name: "Still Time", size: 22.0)!
-        let attributes = [NSFontAttributeName: font]
-        UINavigationBar.appearance().titleTextAttributes = attributes
+        let font = UIFont(name: "Still Time", size: 25.0)!
+        let textAttributes = [
+            NSFontAttributeName: font,
+            NSForegroundColorAttributeName:UIColor.airtimeColors.yellow
+        ]
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+        UINavigationBar.appearance().barTintColor = UIColor.airtimeColors.lightGrey
         
         return true
     }
