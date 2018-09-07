8a789a4b7060bf35af0cfc64236c96fa2fc306db
JoshuaPaulsen <jpaulsen3110@gmail.com>
2018-09-07 11:06:04 -0400

    created color struct

--- AirTime/Colors.swift
+++ AirTime/Colors.swift
@@ -8,5 +8,14 @@
 
 import Foundation
 
+
+extension UIColor {
+    struct airtimeColors {
+        static let yellow = UIColor(red: 0.95, green: 0.80, blue: 0.02, alpha: 1.0)
+        static let lightGrey = UIColor(red: 0.11, green: 0.12, blue: 0.12, alpha: 1.0)
+        static let orange = UIColor(red: 0.97, green: 0.44, blue: 0.11, alpha: 1.0)
+        static let lightOrange = UIColor(red: 0.96, green: 0.56, blue: 0.08, alpha: 1.0)
+    }
+}
                                                                                          
 
