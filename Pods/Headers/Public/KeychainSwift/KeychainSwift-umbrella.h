695213df0d3b940149cdc46a8f5ee90a9e9f53a7
JoshuaPaulsen <jpaulsen3110@gmail.com>
2018-10-29 11:25:18 -0400

    added readme asset

--- Pods/Headers/Public/KeychainSwift/KeychainSwift-umbrella.h
+++ Pods/Headers/Public/KeychainSwift/KeychainSwift-umbrella.h
@@ -1,1 +1,16 @@
-/Users/lincolnfraley/Developer/dynepic/AirTime/Pods/Target Support Files/KeychainSwift/KeychainSwift-umbrella.h
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double KeychainSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char KeychainSwiftVersionString[];
+
